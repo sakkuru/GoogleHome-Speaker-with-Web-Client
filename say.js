ba06044e42d37fc559d696ab7f26a5bdb8c349b0
sakkuru <sakkuru0323@gmail.com>
2018-04-25 13:27:34 -0700

    cut the text if the length is over 200

--- say.js
+++ say.js
@@ -6,10 +6,11 @@ const defaultLang = argv[3] || 'en';
 const device = argv[4] || '';
 
 const notify = (text = defaultText, lang = defaultLang) => {
+    if (text.length > 200) {
+        text = text.slice(0, 200);
+    }
     home.device(device, lang);
-    home.notify(text, (res) => {
-        console.log('Said ' + text);
-    });
+    home.notify(text, (res) => {});
 }
 
 module.exports.say = notify;
